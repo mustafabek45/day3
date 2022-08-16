08194cfe47af2595bf5751bfa391251d623da9f6
Mustafa Bek <bekmustafa45@gmail.com>
2022-08-16 19:46:02 +0200

    B1

--- src/Day3GitGitHub.java
+++ src/Day3GitGitHub.java
@@ -2,6 +2,7 @@ public class Day3GitGitHub {
     public static void main(String[] args) {
         System.out.println("3.Gun 1. deneme");
         System.out.println("3. gund 2. deneme");
+        System.out.println("3. gun 1.branch deneme");
 
 
     }
